48acf544695d525c8575763928a244c71a021f07
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-29 15:52:49 +0000

    Deploy Michael-F-Bryan/tinyvm-rs to github.com/Michael-F-Bryan/tinyvm-rs.git:gh-pages

---
+++ search-index.js
@@ -0,0 +1,5 @@
+var N=null,E="",T="t",U="u",searchIndex={};
+var R=[];
+
+searchIndex["tinyvm"]={"doc":"A Rust port of [jakogut/tinyvm][tinyvm].","i":[],"p":[]};
+initSearch(searchIndex);addSearchOptions(searchIndex);
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["tinyvm"] = {"name":"","dirs":[],"files":["lib.rs"]};
+createSourceSidebar();
